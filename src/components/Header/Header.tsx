c523ee272f208c61a275c2d966c2a4af7c0a4ee4
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-09-17 22:32:25 +0300

    add navigation header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,37 @@
-import { Button, Container, Nav, Navbar, NavLink } from "react-bootstrap"
-import { faVideoSlash } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { faSearch, faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 export const Header = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="/" style={{"color":'gold'}}>
-                    <FontAwesomeIcon icon={faVideoSlash}/>Gold
+            <Container className="header-container">
+                <Navbar.Brand href="/" style={{ color: 'gold' }}>
+                    <FontAwesomeIcon icon={faVideoSlash} />Gold
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0" style={{maxHeight:'100px'}} navbarScroll>
-                        <NavLink className="nav-link">Home</NavLink>
-                        <NavLink className="nav-link">Watch List</NavLink>
-                    </Nav>
-                    <Button variant="outline-info" className="me-2">Log In</Button>
-                    <Button variant="outline-info">Register</Button>
-                </Navbar.Collapse>
+
+                <Nav className="link-group" navbarScroll>
+                    <NavLink to="/" className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/movies" className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}>
+                        Movies
+                    </NavLink>
+                    <NavLink to="/categories" className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}>
+                        Categories
+                    </NavLink>
+                </Nav>
+
+                <div className="info">
+                    <NavLink to="/search" className="search">
+                        <FontAwesomeIcon icon={faSearch} size="lg" />
+                    </NavLink>
+                    <Button variant="outline-info" className="login">Log In</Button>
+                    <Button variant="outline-info" className="signup">Register</Button>
+                </div>
             </Container>
         </Navbar>
-    )
+    );
 }
