5b56fa32841f9afe9db90f581a2161d1a41333c8
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-09-20 19:37:41 +0300

    update header for phone screen

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,22 +2,32 @@ import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { faSearch, faVideoSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink, useNavigate } from "react-router-dom";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import "./Header.css";
+import { useState } from "react";
 
 export const Header = () => {
     const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const handleAuthorize = () => {
         navigate("/authorize")
     }
+
+    const toggleMenu = () => {
+        setMenuOpen(prev => !prev);
+    }
+
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container className="header-container">
-                <Navbar.Brand href="/" style={{ color: 'gold' }}>
-                    <FontAwesomeIcon icon={faVideoSlash} />Gold
+                <Navbar.Brand href="/" style={{ color: 'white' }}>
+                    <FontAwesomeIcon icon={faVideoSlash} />MovieBox
                 </Navbar.Brand>
+                
+                <Nav className={`link-group ${menuOpen ? 'open' : ''}`} navbarScroll>
 
-                <Nav className="link-group" navbarScroll>
                     <NavLink to="/" className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}>
                         Home
                     </NavLink>
@@ -37,6 +47,7 @@ export const Header = () => {
                         <FontAwesomeIcon icon={faSearch} size="lg" />
                     </NavLink>
                     <Button variant="outline-info" className="signup" onClick={handleAuthorize}>Authorize</Button>
+                    <FontAwesomeIcon icon={faBars} onClick={toggleMenu} className="menu-icon" />
                 </div>
             </Container>
         </Navbar>
