d0f29c5bd54b743f259e5e1460e375704ffb9da6
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-09-22 19:55:03 +0300

    add filter and sort button

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,7 +1,7 @@
-import  { CSSProperties, useEffect, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 import { Movie } from '../types/Movie';
-import './Movie.css'
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import './Movie.css';
+import { faFilter, faPlay, faSort } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Paper } from '@mui/material';
 import Carousel from 'react-material-ui-carousel';
@@ -10,16 +10,24 @@ import { Link } from 'react-router-dom';
 interface CustomCSSProperties extends CSSProperties {
     "--img"?: string;
 }
-  
+
 export const Movies = () => {
-    const [movies, setMovies] = useState<Movie[]>([]); 
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [randomMovies, setRandomMovies] = useState<Movie[]>([]);
 
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const response = await fetch('http://localhost:8080/api/V1/movies');
-                const data: Movie[] = await response.json(); 
+                const data: Movie[] = await response.json();
                 setMovies(data);
+                // Вибір випадкових 5 фільмів
+                if (data.length > 5) {
+                    const shuffled = data.sort(() => 0.5 - Math.random());
+                    setRandomMovies(shuffled.slice(0, 5));
+                } else {
+                    setRandomMovies(data);
+                }
             } catch (error) {
                 console.error('Error fetching movies:', error);
             }
@@ -30,56 +38,52 @@ export const Movies = () => {
 
     return (
         <div className='movie-container'>
-            <div className ='movie-carousel-container'>
+            <div className='movie-carousel-container'>
                 <Carousel className='custom-carousel'>
-                {
-                    movies?.map((movie) =>{
-                        return(
-                            <Paper key={movie.imdbId}>
-                                <div className = 'movie-card-container'>
-                                    <div className="movie-card" style={{ "--img": `url(${movie.backdrops[0]})` } as CustomCSSProperties}>
-                                        <div className="movie-detail">
-
-                                        
-
-                                        <div className='movie'>
-                                            <div className="movie-poster">
-                                                <img src={movie.poster} alt="" />
-                                            </div>
-                                            <div className="movie-title">
-                                                <h4>{movie.title}</h4>
-                                                <div className="movie-buttons-container">
-                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                                                    <div className="play-button-icon-container">
-                                                        <FontAwesomeIcon className="play-button-icon"
-                                                            icon = {faPlay}
-                                                        />
-                                                        Start watch
+                    {
+                        randomMovies.map((movie) => {
+                            return (
+                                <Paper key={movie.imdbId}>
+                                    <div className='movie-card-container'>
+                                        <div className="movie-card" style={{ "--img": `url(${movie.backdrops[0]})` } as CustomCSSProperties}>
+                                            <div className="movie-detail">
+                                                <div className='movie'>
+                                                    <div className="movie-poster">
+                                                        <img src={movie.poster} alt="" />
                                                     </div>
-                                                </Link>
+                                                    <div className="movie-title">
+                                                        <h4>{movie.title}</h4>
+                                                        <div className="movie-buttons-container">
+                                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                                <div className="play-button-icon-container">
+                                                                    <FontAwesomeIcon className="play-button-icon" icon={faPlay} />
+                                                                    Start watch
+                                                                </div>
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            </div>
-                                        </div>
-                                        
-                                        
-
                                         </div>
                                     </div>
-                                </div>
-                            </Paper>
-                        )
-                    })
-                }
+                                </Paper>
+                            )
+                        })
+                    }
                 </Carousel>
             </div>
-            {/* <div className='movies-list'>
+            <div className='filter-container'>
+                <div className='sort-button-icon-container'><FontAwesomeIcon className='sort-button-icon' icon={faSort}/>Sort</div>
+                <div className='filter-button-icon-container'><FontAwesomeIcon className='filter-button-icon' icon={faFilter}/>Filter</div>
+            </div>
+            <div className='movies-list'>
                 {movies.map((movie) => (
                     <div className="movie-card-list" key={movie.imdbId}>
                         <img src={movie.poster} alt={movie.title} className="movie-poster-list" />
                         <h3 className="movie-title-list">{movie.title}</h3>
                     </div>
                 ))}
-            </div> */}
-      </div>
+            </div>
+        </div>
     );
 };
